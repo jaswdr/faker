6fae6dfaabead3644174c8037d98dc57094a586d
Jonathan A. Schweder <jonathanschweder@gmail.com>
2018-08-09 14:55:53 -0300

    Removed use of pointer in person.go

--- person.go
+++ person.go
@@ -105,19 +105,19 @@ type Person struct {
 	Faker *Faker
 }
 
-func (p *Person) Suffix() string {
+func (p Person) Suffix() string {
 	return suffix[p.Faker.IntBetween(0, len(suffix)-1)]
 }
 
-func (p *Person) TitleMale() string {
+func (p Person) TitleMale() string {
 	return "Mr."
 }
 
-func (p *Person) TitleFemale() string {
+func (p Person) TitleFemale() string {
 	return "Ms."
 }
 
-func (p *Person) Title() string {
+func (p Person) Title() string {
 	if p.Faker.IntBetween(0, 1) == 0 {
 		return p.TitleMale()
 	}
@@ -125,27 +125,27 @@ func (p *Person) Title() string {
 	return p.TitleFemale()
 }
 
-func (p *Person) FirstNameMale() string {
+func (p Person) FirstNameMale() string {
 	index := p.Faker.IntBetween(0, len(firstNameMale)-1)
 	return firstNameMale[index]
 }
 
-func (p *Person) FirstNameFemale() string {
+func (p Person) FirstNameFemale() string {
 	index := p.Faker.IntBetween(0, len(firstNameFemale)-1)
 	return firstNameFemale[index]
 }
 
-func (p *Person) FirstName() string {
+func (p Person) FirstName() string {
 	names := append(firstNameMale, firstNameFemale...)
 	return p.Faker.RandomStringElement(names)
 }
 
-func (p *Person) LastName() string {
+func (p Person) LastName() string {
 	index := p.Faker.IntBetween(0, len(lastName)-1)
 	return lastName[index]
 }
 
-func (p *Person) Name() string {
+func (p Person) Name() string {
 	formats := append(maleNameFormats, femaleNameFormats...)
 	name := formats[p.Faker.IntBetween(0, len(formats)-1)]
 
