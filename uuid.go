5f00643ee55d20bb6553c1fc953ae33e4d33c6bd
Jonathan A. Schweder <jonathanschweder@gmail.com>
2018-08-10 14:15:42 -0300

    Corrected generation of UUID v4

--- uuid.go
+++ uuid.go
@@ -1,38 +1,19 @@
 package faker
 
+import (
+	"crypto/rand"
+	"fmt"
+	"io"
+)
+
 type UUID struct {
 	Faker *Faker
 }
 
-func (u UUID) randomAFNumber() string {
-	group := []string{
-		"a", "b", "c", "d", "e", "f",
-		"A", "B", "C", "D", "E", "F",
-		"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-	}
-
-	return u.Faker.RandomStringElement(group)
-}
-
-func (u UUID) randomAFNumberCount(count int) (result string) {
-	for i := 0; i < count; i++ {
-		result += u.randomAFNumber()
-	}
-
-	return
-}
-
 func (u UUID) V4() (uuid string) {
-	uuid += u.randomAFNumberCount(8)
-	uuid += "-"
-	uuid += u.randomAFNumberCount(4)
-	uuid += "-"
-	uuid += "4"
-	uuid += u.randomAFNumberCount(3)
-	uuid += "-"
-	uuid += u.Faker.RandomStringElement([]string{"8", "9", "a", "A", "b", "B"})
-	uuid += u.randomAFNumberCount(3)
-	uuid += "-"
-	uuid += u.randomAFNumberCount(12)
-	return
+	var uiq [16]byte
+	io.ReadFull(rand.Reader, uiq[:])
+	uiq[6] = (uiq[6] & 0x0f) | 0x40           // Version 4
+	uiq[8] = (uiq[8]&(0xff>>2) | (0x02 << 6)) // Variant RFC4122
+	return fmt.Sprintf("%x%x%x%x%x", uiq[0:4], uiq[4:6], uiq[6:8], uiq[8:10], uiq[10:])
 }
