2214ae08d9c17a8d68077e83391224c14576dc4c
Jonathan A. Schweder <jonathanschweder@gmail.com>
2019-07-24 19:49:14 -0300

    Removed log message in uuid_test.go

--- uuid_test.go
+++ uuid_test.go
@@ -9,7 +9,6 @@ func TestUUIDv4(t *testing.T) {
 	f := New()
 	value := f.UUID().V4()
 	match, err := regexp.MatchString("^[a-fA-F0-9]{8}[a-fA-F0-9]{4}4[a-fA-F0-9]{3}[8|9|aA|bB][a-fA-F0-9]{3}[a-fA-F0-9]{12}$", value)
-	t.Log(value)
 	Expect(t, true, err == nil)
 	Expect(t, true, match)
 }
