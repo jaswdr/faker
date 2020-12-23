8406c66ae5e609f0eb222a2d2788c461fefb9f93
Jonathan Schweder <jonathanschweder@gmail.com>
2020-12-23 10:00:08 +0000

    Add LanguageAbbr and ProgrammingLanguage to Language generator

--- language_test.go
+++ language_test.go
@@ -8,3 +8,13 @@ func TestLanguage(t *testing.T) {
 	v := New().Language().Language()
 	NotExpect(t, "", v)
 }
+
+func TestLanguageAbbr(t *testing.T) {
+	v := New().Language().LanguageAbbr()
+	NotExpect(t, "", v)
+}
+
+func TestProgrammingLanguage(t *testing.T) {
+	v := New().Language().ProgrammingLanguage()
+	NotExpect(t, "", v)
+}
