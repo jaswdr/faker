2d84d32164005bc6b26ccbf3875ed959f3761b72
FournyP <pierre.fourny76@gmail.com>
2021-04-18 19:46:47 +0200

    Creating and starting to implement loremflickr

---
+++ loremflickr.go
@@ -0,0 +1,49 @@
+package faker
+
+import (
+	"net/http"
+	"os"
+)
+
+const BASE_URL = "https://loremflickr.com"
+
+type LoremFlickr struct {
+	faker *Faker
+}
+
+func (lf LoremFlickr) Image(width, height int, categories []string, prefix string, categoriesStrict bool) *os.File {
+
+	url := BASE_URL
+
+	switch prefix {
+	case "g":
+		url += "/g"
+	case "p":
+		url += "/p"
+	case "red":
+		url += "/red"
+	case "green":
+		url += "/green"
+	case "blue":
+		url += "/blue"
+	}
+
+	url += string('/') + string(width) + string('/') + string(height)
+
+	if len(categories) > 0 {
+
+		url += string('/')
+
+		for _, category := range categories {
+			url += category + string(',')
+		}
+
+		if categoriesStrict {
+			url += "/all"
+		}
+	}
+
+	http.Get(url)
+
+	return &os.File{}
+}
