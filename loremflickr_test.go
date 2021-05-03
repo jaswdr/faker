8405cab63607f26da38e2cd68f293b69b21f628d
Jonathan Schweder <jonathanschweder@gmail.com>
2021-05-03 12:59:26 +0100

    Adding generator for LoremFlickr (#53)

---
+++ loremflickr_test.go
@@ -0,0 +1,14 @@
+package faker
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestImageLorem(t *testing.T) {
+	f := New()
+	value := f.LoremFlickr().Image(300, 200, []string{}, "", false)
+	Expect(t, fmt.Sprintf("%T", value), "*os.File")
+	Expect(t, strings.HasSuffix(value.Name(), ".jpg"), true, value.Name())
+}
