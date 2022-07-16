e683f8deff33c49fd58480f19006c62a984c1a9b
Wako <developer.rodrigo.patino@gmail.com>
2022-07-16 09:38:14 -0500

    Improove performace using a string builders instead of a string (#102)
    Thank you very much for the improvement and congrats for your first contribution.
    
    LGTM, merging.

--- binarystring.go
+++ binarystring.go
@@ -1,15 +1,17 @@
 package faker
 
-//BinaryString is the faker struct for BinaryString
+import "strings"
+
+// BinaryString is the faker struct for BinaryString
 type BinaryString struct {
 	faker *Faker
 }
 
 // BinaryString returns a random binarystring of given input length
 func (bn BinaryString) BinaryString(length int) string {
-	var bs string
+	var bs strings.Builder
 	for i := 0; i < length; i++ {
-		bs += bn.faker.RandomStringElement([]string{"0", "1"})
+		bs.WriteString(bn.faker.RandomStringElement([]string{"0", "1"}))
 	}
-	return bs
+	return bs.String()
 }
