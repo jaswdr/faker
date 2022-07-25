1bfb09225a58189ff5cd4f0975d8584a8ab9b092
Maodo DIOP <dmaodo@gmail.com>
2022-07-25 18:22:06 +0000

    Update blood_test.go

--- blood_test.go
+++ blood_test.go
@@ -7,11 +7,7 @@ import (
 func TestBloodName(t *testing.T) {
 	v := New().Blood().Name()
 	NotExpect(t, "", v)
-	Expect(t, true, v == "A+" || v == "A-" || v == "B+" || v == "B-" || v == "AB+" || v == "AB-" || v == "O+" || v == "O-")
+	ExpectInString(t, v, bloodTypes)
 }
 
-func TestBloodAbbr(t *testing.T) {
-	v := New().Blood().Abbr()
-	NotExpect(t, "", v)
-	Expect(t, true, v == "A+" || v == "A-" || v == "B+" || v == "B-" || v == "AB+" || v == "AB-" || v == "O+" || v == "O-")
-}
+
