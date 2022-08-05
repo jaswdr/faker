a46ba4766dda130c9ff58287b3d171dd5af0cd4a
Aaron R <33096067+adrubesh@users.noreply.github.co>
2022-08-05 12:30:29 -0500

    Added fake Directories to file.go

--- file.go
+++ file.go
@@ -1,6 +1,10 @@
 package faker
 
-import "fmt"
+import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+)
 
 var (
 	extensions = []string{"ods", "xls", "xlsx", "csv", "ics", "vcf", "3dm", "3ds", "max", "bmp", "dds", "gif", "jpg", "jpeg", "png", "psd", "xcf", "tga", "thm", "tif", "tiff", "yuv", "ai", "eps", "ps", "svg", "dwg", "dxf", "gpx", "kml", "kmz", "webp", "3g2", "3gp", "aaf", "asf", "avchd", "avi", "drc", "flv", "m2v", "m4p", "m4v", "mkv", "mng", "mov", "mp2", "mp4", "mpe", "mpeg", "mpg", "mpv", "mxf", "nsv", "ogg", "ogv", "ogm", "qt", "rm", "rmvb", "roq", "srt", "svi", "vob", "webm", "wmv", "yuv", "aac", "aiff", "ape", "au", "flac", "gsm", "it", "m3u", "m4a", "mid", "mod", "mp3", "mpa", "pls", "ra", "s3m", "sid", "wav", "wma", "xm", "7z", "a", "apk", "ar", "bz2", "cab", "cpio", "deb", "dmg", "egg", "gz", "iso", "jar", "lha", "mar", "pea", "rar", "rpm", "s7z", "shar", "tar", "tbz2", "tgz", "tlz", "war", "whl", "xpi", "zip", "zipx", "xz", "pak", "exe", "msi", "bin", "command", "sh", "bat", "crx", "c", "cc", "class", "clj", "cpp", "cs", "cxx", "el", "go", "h", "java", "lua", "m", "m4", "php", "pl", "po", "py", "rb", "rs", "sh", "swift", "vb", "vcxproj", "xcodeproj", "xml", "diff", "patch", "html", "js", "html", "htm", "css", "js", "jsx", "less", "scss", "wasm", "php", "eot", "otf", "ttf", "woff", "woff2", "ppt", "odp", "doc", "docx", "ebook", "log", "md", "msg", "odt", "org", "pages", "pdf", "rtf", "rst", "tex", "txt", "wpd", "wps", "mobi", "epub", "azw1", "azw3", "azw4", "azw6", "azw", "cbr", "cbz"}
@@ -22,4 +26,28 @@ func (f File) FilenameWithExtension() string {
 	text := f.Faker.Lorem().Word()
 
 	return fmt.Sprintf("%s.%s", text, extension)
+}
+
+// AbsoluteUnixPath returns a fake absolute unix path to a file
+func (f File) AbsoluteUnixPath(nbLen int) string {
+	return filepath.Join(f.Directory(nbLen), f.FilenameWithExtension())
+}
+
+// AbsoluteWin32Path returns a fake absolute win32 path to a file
+func (f File) AbsoluteWin32Path(nbLen int) string {
+	return filepath.Join(f.DriveLetter(), f.Directory(nbLen), f.FilenameWithExtension())
+}
+
+// Directory returns a fake directory (the directory path style OS dependent)
+func (f File) Directory(nbLen int) string {
+	if runtime.GOOS == "windows" {
+		return f.DriveLetter() + filepath.Join(f.Faker.Lorem().Words(nbLen)...)
+	} else {
+		return "/" + filepath.Join(f.Faker.Lorem().Words(nbLen)...)
+	}
+}
+
+// DriveLetter returns a fake Win32 drive letter
+func (f File) DriveLetter() string {
+	return f.Faker.RandomLetter() + ":\\"
 }
--- file_test.go
+++ file_test.go
@@ -1,6 +1,9 @@
 package faker
 
 import (
+	"path/filepath"
+	"regexp"
+	"runtime"
 	"strings"
 	"testing"
 )
@@ -13,4 +16,24 @@ func TestExtension(t *testing.T) {
 func TestFileWithExtension(t *testing.T) {
 	p := New().File()
 	Expect(t, true, len(strings.Split(p.FilenameWithExtension(), ".")) == 2)
+}
+
+func TestDirectory(t *testing.T) {
+	p := New().File()
+	Expect(t, true, len(strings.Split(p.Directory(2), string(filepath.Separator))) == 3)
+}
+
+func TestDriveLetter(t *testing.T) {
+	p := New().File()
+	reg := regexp.MustCompile(`^[a-zA-Z]:\\`)
+	Expect(t, true, reg.MatchString(p.DriveLetter()))
+}
+
+func TestAbsolutePath(t *testing.T) {
+	p := New().File()
+	if runtime.GOOS == "windows" {
+		Expect(t, true, filepath.IsAbs(p.AbsoluteWin32Path(3)))
+	} else {
+		Expect(t, true, filepath.IsAbs(p.AbsoluteUnixPath(3)))
+	}
 }
