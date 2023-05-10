5d0f88f64c127972c97969694d328cbfe612655f
Pankaj Kumar <pankaj-kumar34@users.noreply.github.com>
2023-05-10 21:05:26 +0530

    fix: UUID V4 generation (#138)

--- uuid.go
+++ uuid.go
@@ -17,5 +17,5 @@ func (UUID) V4() (uuid string) {
 	io.ReadFull(rand.Reader, uiq[:])
 	uiq[6] = (uiq[6] & 0x0f) | 0x40           // Version 4
 	uiq[8] = (uiq[8]&(0xff>>2) | (0x02 << 6)) // Variant RFC4122
-	return fmt.Sprintf("%x%x%x%x%x", uiq[0:4], uiq[4:6], uiq[6:8], uiq[8:10], uiq[10:])
+	return fmt.Sprintf("%x-%x-%x-%x-%x", uiq[0:4], uiq[4:6], uiq[6:8], uiq[8:10], uiq[10:])
 }
