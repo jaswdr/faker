9d74c7b95eb501fb5f344df1100ff3269fc0cbfb
Antoine Froger <antfroger@gmail.com>
2024-04-24 17:20:14 +0200

    Fix credit card expiration date
    The generated expiration dates were in date/month format, while dates should be in month/year format.

--- payment.go
+++ payment.go
@@ -2,6 +2,7 @@ package faker
 
 import (
 	"fmt"
+	"time"
 )
 
 var cardVendors = []string{
@@ -25,9 +26,11 @@ func (p Payment) CreditCardNumber() string {
 	return p.Faker.Numerify("################")
 }
 
-// CreditCardExpirationDateString returns a fake credit card expiration date in string format for Payment
+// CreditCardExpirationDateString returns a fake credit card expiration date in string format (i.e. mm/yy) for Payment
 func (p Payment) CreditCardExpirationDateString() string {
-	day := p.Faker.IntBetween(0, 30)
-	month := p.Faker.IntBetween(12, 30)
-	return fmt.Sprintf("%02d/%02d", day, month)
+	month := p.Faker.IntBetween(1, 12)
+	currentYear := time.Now().Year()
+	year := p.Faker.IntBetween(currentYear, currentYear+3)
+
+	return fmt.Sprintf("%02d/%02d", month, year%100)
 }
