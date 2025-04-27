92e8e0d536e4f66c042fc4a8fe6c77dfb90fede7
Jonathan Schweder <jonathanschweder@gmail.com>
2025-04-27 19:58:59 +0100

    Use english dictionary for words instead of subset

--- lorem.go
+++ lorem.go
@@ -4,8 +4,6 @@ import (
 	"strings"
 )
 
-var wordsList = []string{"a", "in", "et", "ut", "ut", "ad", "et", "at", "id", "et", "ut", "in", "ab", "ea", "ut", "et", "et", "et", "et", "et", "et", "ea", "id", "et", "et", "ut", "ut", "ex", "est", "sed", "qui", "est", "est", "aut", "eos", "qui", "cum", "nam", "non", "aut", "qui", "sed", "qui", "vel", "non", "sit", "rem", "eos", "qui", "qui", "sed", "est", "non", "est", "sit", "eum", "hic", "quo", "sit", "aut", "aut", "vel", "aut", "eum", "aut", "quo", "odio", "enim", "unde", "illo", "sunt", "quis", "sint", "sint", "quas", "fuga", "modi", "enim", "quos", "odit", "quia", "sunt", "eius", "quia", "quia", "nisi", "iste", "quam", "vero", "amet", "ipsa", "esse", "quis", "quae", "quia", "nemo", "iure", "quod", "illum", "ipsum", "dolor", "rerum", "velit", "culpa", "omnis", "nihil", "minus", "saepe", "iusto", "velit", "magni", "alias", "omnis", "porro", "autem", "nihil", "totam", "fugit", "dolor", "optio", "atque", "autem", "ipsam", "nobis", "nulla", "ullam", "rerum", "harum", "eaque", "error", "animi", "dicta", "vitae", "quasi", "natus", "earum", "rerum", "omnis", "neque", "sequi", "libero", "soluta", "cumque", "beatae", "maxime", "facere", "quidem", "labore", "dolore", "veniam", "minima", "fugiat", "itaque", "magnam", "dolorem", "laborum", "nostrum", "quaerat", "officia", "maiores", "facilis", "dolorem", "aliquam", "numquam", "aliquid", "dolorum", "aperiam", "tempore", "dolores", "eveniet", "dolores", "debitis", "commodi", "tempora", "ratione", "ducimus", "tenetur", "placeat", "impedit", "quisquam", "nesciunt", "adipisci", "pariatur", "deleniti", "voluptas", "incidunt", "repellat", "eligendi", "possimus", "corporis", "expedita", "sapiente", "delectus", "suscipit", "voluptas", "deserunt", "mollitia", "corrupti", "voluptas", "officiis", "accusamus", "similique", "doloribus", "provident", "occaecati", "quibusdam", "assumenda", "inventore", "veritatis", "explicabo", "voluptate", "molestiae", "molestias", "excepturi", "molestiae", "recusandae", "asperiores", "voluptatem", "reiciendis", "laudantium", "voluptatem", "temporibus", "voluptatum", "voluptatem", "laboriosam", "aspernatur", "voluptates", "voluptatem", "distinctio", "architecto", "cupiditate", "doloremque", "blanditiis", "dignissimos", "repellendus", "consequatur", "accusantium", "consectetur", "repudiandae", "consequatur", "praesentium", "perferendis", "consequatur", "voluptatibus", "perspiciatis", "consequuntur", "reprehenderit", "necessitatibus", "exercitationem"}
-
 // Lorem is a faker struct for Lorem
 type Lorem struct {
 	Faker *Faker
@@ -13,8 +11,8 @@ type Lorem struct {
 
 // Word returns a fake word for Lorem
 func (l Lorem) Word() string {
-	index := l.Faker.IntBetween(0, len(wordsList)-1)
-	return wordsList[index]
+	index := l.Faker.IntBetween(0, len(englishWords)-1)
+	return englishWords[index]
 }
 
 // Words returns fake words for Lorem
@@ -59,7 +57,7 @@ func (l Lorem) Paragraphs(nbParagraph int) []string {
 
 // Text returns a fake text for Lorem
 func (Lorem) Text(maxNbChars int) (out string) {
-	for _, w := range wordsList {
+	for _, w := range englishWords {
 		if len(out)+len(w) > maxNbChars {
 			break
 		}
