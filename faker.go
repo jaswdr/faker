8e8cb04aa6455a023e01eff7c1236cb9a4201ec1
Jonathan Schweder <jonathanschweder@gmail.com>
2025-08-05 21:43:06 +0100

    Fix unused variable

--- faker.go
+++ faker.go
@@ -292,7 +292,6 @@ func handleFullRange[T number](minN, maxN T, rand GeneratorInterface) T {
 	// Split range: 50% negative, 50% positive to avoid overflow
 	if rand.Intn(2) == 0 {
 		// Generate negative number in range [minN, 0]
-		maxVal := maxInt(maxN)
 		value := generateMaxRangeValue[T](maxN, rand)
 		return minN + value
 	}
